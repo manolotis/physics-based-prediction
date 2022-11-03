9589a6b00e119b482d7de8ad005e3f0f2957fc7a
Manu <manuel.munoz.sanchez@gmail.com>
2022-11-03 14:38:12 +0100

    Evaluation scripts

---
+++ code/bash/rdlm_evaluate_all.sh
@@ -0,0 +1,5 @@
+CONFIG="/home/manolotis/sandbox/robustness_benchmark/physicsBased/code/configs/evaluate_all.yaml"
+SCRIPT="/home/manolotis/sandbox/robustness_benchmark/code/evaluate.py"
+
+python $SCRIPT --config $CONFIG
+
